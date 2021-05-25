35fcc59323a48bc255905043758dd78eb09b220c
MrKatsee <ghtjd000129@naver.com>
2021-05-25 23:47:42 +0900

    # 씬 설정 변경

--- TTDProject/Assets/Scripts/MyBuilder.cs
+++ TTDProject/Assets/Scripts/MyBuilder.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MyBuilder : MonoBehaviour
-{
+public class MyBuilder : MonoBehaviour {
     private static MyBuilder _instance = null;
     public static MyBuilder Instance { get { return _instance; } }
 
@@ -23,22 +22,17 @@ public class MyBuilder : MonoBehaviour
 
     private static IEnumerator buildRoutine;
 
-    public void Build(MyTower tower)
-    {
-        if (buildRoutine == null)
-        {
-            if (TimeManager.Instance.time >= tower.cost)
-            {
+    public void Build(MyTower tower) {
+        if (buildRoutine == null) {
+            if (TimeManager.Instance.time >= tower.cost) {
                 buildRoutine = BuildRoutine(tower);
                 StartCoroutine(buildRoutine);
             }
         }
     }
 
-    public void Remove()
-    {
-        if(buildRoutine == null)
-        {
+    public void Remove() {
+        if (buildRoutine == null) {
             buildRoutine = RemoveRoutine();
             StartCoroutine(buildRoutine);
         }
@@ -53,15 +47,12 @@ public class MyBuilder : MonoBehaviour
         }
     }
 
-    private IEnumerator RemoveRoutine()
-    {
+    private IEnumerator RemoveRoutine() {
         bool isEnd = false;
-        while (isEnd == false)
-        {
+        while (isEnd == false) {
             yield return new WaitUntil(() => Input.GetMouseButtonDown(0) && EventSystem.current.IsPointerOverGameObject() == false);
             Collider2D[] colliderArray = Physics2D.OverlapPointAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), LayerMask.GetMask("BuildArea"));
-            if(colliderArray.Length > 0)
-            {
+            if (colliderArray.Length > 0) {
                 MyTower target = colliderArray[0].GetComponent<MyTower>();
                 target?.Remove();
                 isEnd = true;
@@ -105,44 +96,35 @@ public class MyBuilder : MonoBehaviour
         buildRoutine = null;
     }
 
-    private IEnumerator BuildRoutine(MyTower tower)
-    {
+    private IEnumerator BuildRoutine(MyTower tower) {
         WaitUntil uiCheck = new WaitUntil(() => EventSystem.current.IsPointerOverGameObject() == false);
         yield return uiCheck;
         MyTower newTower = Instantiate(tower, _towerParent);
         bool isBuild = false;
         newTower.ShowBuildUI();
-        while(isBuild == false)
-        {
+        while (isBuild == false) {
             yield return uiCheck;
             newTower.transform.localPosition = grid.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            if (newTower.CheckArea())
-            {
-                if (Input.GetMouseButton(0))
-                {
+            if (newTower.CheckArea()) {
+                if (Input.GetMouseButton(0)) {
                     isBuild = true;
                     break;
                 }
             }
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
                 break;
             }
-            if(TimeManager.Instance.time < newTower.cost)
-            {
+            if (TimeManager.Instance.time < newTower.cost) {
                 break;
             }
             yield return null;
         }
 
-        if (isBuild)
-        {
+        if (isBuild) {
             newTower.HideBuildUI();
             newTower.Init();
             TimeManager.Instance.time -= newTower.cost;
-        }
-        else
-        {
+        } else {
             Destroy(newTower.gameObject);
         }
         buildRoutine = null;
