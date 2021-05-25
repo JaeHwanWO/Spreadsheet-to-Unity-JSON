9ef6ca027c9b5ed3e826e3af868f56ce6611eb50
MrKatsee <ghtjd000129@naver.com>
2021-05-25 21:56:30 +0900

    # 데모 프로젝트 로드

---
+++ TTDProject/Assets/Scripts/Data/MonsterManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class MonsterManager : MonoBehaviour
+{
+    public static MonsterManager Instance { get; private set; } = null;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        Init();
+    }
+
+    public GameObject monsterPrefab;
+    public WaveDatabase waveDB;
+    public MonsterDatabase monsterDB;
+    public MySpawner[] spawner;
+
+    public Text text;
+
+    public Monster InstantiateMonster()
+    {
+        Monster monster = Instantiate(monsterPrefab).GetComponent<Monster>();
+        return monster;
+    }
+
+    private void Init()
+    {
+        StartCoroutine(GameRoutine()); //임시
+    }
+
+    int maxWave;        
+    IEnumerator GameRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+
+        int wave = 0;
+        int turn = 0;
+
+        int spawnerId = 0;
+        int monsterId = 0;
+        int spawnDelay = 0;
+
+        while(true)
+        {
+            Debug.Log($"Now Wave : {wave}");
+
+            if (wave > maxWave)
+            {
+                wave = 0;
+            }
+
+            turn = 0;
+
+            WaveData waveData = waveDB.GetData(wave);
+
+            while(true)
+            {
+                if (turn >= waveData.maxTurn)
+                {
+                    break;
+                }
+                Debug.Log($"turn : {turn} / maxTurn : {waveData.maxTurn}");
+
+                TurnInfo turnInfo = waveData.GetTurnInfo(turn);
+
+                spawnerId = turnInfo.spawnerID;
+                monsterId = turnInfo.monsterID;
+                spawnDelay = turnInfo.delay;
+
+                spawner[spawnerId].Spawn(monsterDB.GetData(monsterId));
+
+                yield return new WaitForSeconds(spawnDelay);
+
+                turn++;
+            }
+
+            wave++;
+            TimeManager.Instance.time += 100;
+
+            yield return null;
+            //yield return WaitUntil 준비 기간 동안 정지
+        }
+    }
+
+    public void GameOver()
+    {
+        StopAllCoroutines();
+    }
+
+    public void GameStart()
+    {
+        Init();
+    }
+}
---
+++ TTDProject/Assets/Scripts/Data/TowerData.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public enum BulletType {
+    Normal = 0, Dot = 1,
+}
+
+public class TowerData : ScriptableObject {
+    [SerializeField] int _sprite_Base;
+    public int sprite_Base { get { return _sprite_Base; } }
+    [SerializeField] int _sprite_Barrel;
+    public int sprite_Barrel { get { return _sprite_Barrel; } }
+
+    [SerializeField] int _cost;
+    public int cost { get { return _cost; } }
+
+    [SerializeField] float _atkRange;
+    public float atkRange { get { return _atkRange; } }
+
+    [SerializeField] float _atkDelay;
+    public float atkDelay { get { return _atkDelay; } }
+    [SerializeField] int _atkCount;
+    public int atkCount { get { return _atkCount; } }
+
+    [SerializeField] float _bulletPos;
+    public float bulletPos { get { return _bulletPos; } }
+
+    [SerializeField] BulletType _bulletType;
+    public BulletType bulletType { get { return _bulletType; } }
+    [SerializeField] int _bulletDamage;
+    public int bulletDamage { get { return _bulletDamage; } }
+}
+
+public class TowerDataGenerator {
+    private const string _defaultFileName = "NewTowerData";
+    private const string _extension = ".asset";
+
+    [MenuItem("Assets/Create/Sheet2Unity/Demo/TowerData")]
+    public static void GenerateEditor() {
+        ScriptableObject scriptableObject = ScriptableObject.CreateInstance<TowerData>();
+
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == "") {
+            path = "Assets";
+        } else if (Path.GetExtension(path) != "") {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+        }
+
+        AssetDatabase.CreateAsset(scriptableObject, path + "/" + _defaultFileName + _extension);
+        AssetDatabase.SaveAssets();
+
+        Selection.activeObject = scriptableObject;
+    }
+}
